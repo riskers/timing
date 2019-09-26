ee5cf3ca8e2e86245228d66256467eab6c14bdc5
riskers <gaoyibobobo@gmail.com>
2019-09-27 02:44:58 +0800

    svg number

--- lib/src/show_text.dart
+++ lib/src/show_text.dart
@@ -1,26 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ShowText extends StatelessWidget {
   final int text;
-  const ShowText(this.text, {Key key}) : super(key: key);
+  const ShowText(this.text, {Key key})
+      : assert(text != null),
+        super(key: key);
 
-  String _fillTime(int x) {
+  List<String> _fillTime(int x) {
+    var res = List<String>();
+
     if (x < 10) {
-      return "0" + x.toString();
+      res.add("0");
+      res.add(x.toString());
+    } else {
+      res.add((x ~/ 10).toString());
+      res.add((x % 10).toString());
     }
 
-    return x.toString();
+    return res;
+  }
+
+  Widget _svgNumber(
+    String n, {
+    @required double width,
+    Color color,
+  }) {
+    return SvgPicture.asset(
+      "assets/$n.svg",
+      color: color,
+      width: width,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      _fillTime(text),
-      style: TextStyle(
-        fontWeight: FontWeight.bold,
-        color: Colors.white,
-        fontSize: 60,
-      ),
+    final size = MediaQuery.of(context).size;
+    final width = size.width;
+    final svgWidth = width / 8;
+
+    var strTime = _fillTime(text);
+    Color color = Colors.white;
+
+    return Row(
+      children: <Widget>[
+        _svgNumber(strTime[0], width: svgWidth, color: color),
+        _svgNumber(strTime[1], width: svgWidth, color: color),
+      ],
     );
   }
 }
